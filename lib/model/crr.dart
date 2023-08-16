a1a9610506a5e632ef4d22bab857193a9d25ee3d
chakssue <rtrt505@korea.ac.kr>
2023-08-17 00:18:08 +0900

    crr.dart 주석 추가

--- lib/model/crr.dart
+++ lib/model/crr.dart
@@ -4,6 +4,7 @@ import 'package:http/http.dart' as http;
 class Score extends StatefulWidget {
   final String initialValue;
 
+  // 생성자로 받아온 초기값에 따라 crr로 비교할 글이 달라짐.
   const Score({Key? key, required this.initialValue}) : super(key: key);
 
   @override
@@ -12,25 +13,28 @@ class Score extends StatefulWidget {
 
 class _ScoreState extends State<Score> {
   late String value;
-  late String url;
-  String crrScore = '';
+  late String url; // API 요청을 보낼 주소
+  String crrScore = ''; // crr 점수를 저장할 변수 (string형, 필요시 double형으로 수정)
 
   @override
   void initState() {
     super.initState();
-    value = widget.initialValue;
-    url = 'https://daitso105.run.goorm.site/crr?query=$value';
+    value = widget.initialValue; // 초기값을 변수에 저장
+    url = 'https://daitso105.run.goorm.site/crr?query=$value'; // API 요청 주소 설정
   }
 
+  // API 요청을 보내고 결과를 처리하는 함수, 이 함수가 실행되면 현 위젯의 crrScore 값이 갱신된다.
   void fetchdata(String url) async {
-    http.Response response = await http.get(Uri.parse(url));
+    http.Response response = await http.get(Uri.parse(url)); // 주어진 주소로 GET 요청
     if (response.statusCode == 200) {
+      // 요청이 성공한 경우
       setState(() {
-        crrScore = response.body;
+        crrScore = response.body; // 받아온 결과를 변수에 저장하여 UI 갱신
       });
     } else {
+      // 요청이 실패한 경우
       setState(() {
-        crrScore = 'Error: ${response.statusCode}';
+        crrScore = 'Error: ${response.statusCode}'; // 에러 메시지를 변수에 저장하여 UI 갱신
       });
     }
   }
@@ -42,15 +46,15 @@ class _ScoreState extends State<Score> {
       children: [
         TextButton(
           onPressed: () async {
-            fetchdata(url);
+            fetchdata(url); // 버튼 클릭 시 fetchdata 함수 실행
           },
           child: const Text(
-            'Compute crr score',
+            'Compute crr score', // 버튼에 표시될 텍스트
             style: TextStyle(fontSize: 15),
           ),
         ),
         Text(
-          crrScore,
+          crrScore, // crr 점수 표시 (현재 string형)
           style: const TextStyle(fontSize: 15, color: Colors.green),
         )
       ],
