f9cbc46074276c6a50ba2a0d11ed057240c76e61
Hongjunior <seokjun09@gmail.com>
2023-08-22 16:50:28 +0900

    Auto stash before merge of "main" and "origin/main"

---
+++ lib/screen/diagnosis_face/diagnosis_kid_13.dart
@@ -0,0 +1,144 @@
+import 'package:bridze/widgets/audio_kid_13.dart';
+import 'package:flutter/material.dart';
+import 'diagnosis_kid_14.dart';
+
+class DiagnosisKid13Page extends StatefulWidget {
+  const DiagnosisKid13Page({Key? key}) : super(key: key);
+
+  @override
+  State<DiagnosisKid13Page> createState() => _DiagnosisKid13PageState();
+}
+
+class _DiagnosisKid13PageState extends State<DiagnosisKid13Page> {
+  AudioKid13 myAudioPlayer = AudioKid13(); // Use AudioKid12 class
+  bool isPlaying = false;
+
+  @override
+  void initState() {
+    super.initState();
+    myAudioPlayer.initAudio().then((_) {
+      setState(() {
+        isPlaying = true;
+        myAudioPlayer.toggleAudio();
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: <Widget>[
+          Image.asset(
+            'assets/images/diagnosis_kid_2.png',
+            fit: BoxFit.cover,
+            height: 1024,
+            width: 1440,
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(
+                  top: 30,
+                  left: 30,
+                ),
+                child: Row(
+                  children: [
+                    Image.asset(
+                      'assets/images/chat.png',
+                      height: 40,
+                    ),
+                    const SizedBox(width: 10),
+                    const Text(
+                      "đánh giá tình cảm",
+                      style: TextStyle(
+                        fontSize: 50,
+                        fontFamily: 'Rowdies',
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 30),
+                child: Center(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Image.asset(
+                        'assets/images/torizzi.png',
+                        width: 250,
+                        height: 250,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              const Center(
+                child: Padding(
+                  padding: EdgeInsets.only(left: 40, top: 30),
+                  child: Text(
+                    "왜 그렇게 생각했어 ?",
+                    style: TextStyle(
+                      fontSize: 40,
+                      fontFamily: 'BMJUA',
+                      color: Color(0xff000000),
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 30, top: 30),
+                child: Center(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      GestureDetector(
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => const DiagnosisKid14Page(),
+                            ),
+                          );
+                        },
+                        child: Image.asset(
+                          'assets/images/camera.png',
+                          width: 40,
+                          height: 40,
+                        ),
+                      ),
+                      const SizedBox(width: 10),
+                      const Text(
+                        "아이의 표정이 촬영됩니다",
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontFamily: 'BMJUA',
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 30, top: 30),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Image.asset(
+                      'assets/images/kid.png',
+                      width: 50,
+                      height: 50,
+                    ),
+                    const SizedBox(width: 10),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
